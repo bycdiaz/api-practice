ebc8d79d29e90b41a044a52ccc4828c7ae152646
Carlos Diaz <cdiaz86@gmail.com>
2020-02-28 21:13:23 -0500

    Adds gif search field.

--- script.js
+++ script.js
@@ -1,12 +1,46 @@
-const img = document.querySelector('img');
-const refreshButton = document.querySelector('.refresh-button');
+const retrieveGif = () => {
+  const img = document.querySelector('img');
+  const refreshButton = document.querySelector('.refresh-button');
 
-fetch('https://api.giphy.com/v1/gifs/translate?api_key=JCi4z2AIWowzJTWX7HtbX7OjQIeZEHpT&s=cats', { mode: 'cors' })
-  .then((response) => response.json())
-  .then((response) => {
-    img.src = response.data.images.original.url;
-  });
+  const setURL = (searchInput) => {
+    let url = '';
+    if (searchInput) {
+      url = `https://api.giphy.com/v1/gifs/translate?api_key=JCi4z2AIWowzJTWX7HtbX7OjQIeZEHpT&s=${searchInput}`;
+    } else {
+      url = 'https://api.giphy.com/v1/gifs/translate?api_key=JCi4z2AIWowzJTWX7HtbX7OjQIeZEHpT&s=cats';
+    }
+    return url;
+  };
 
-refreshButton.addEventListener('click', () => {
-  window.location.reload();
-});
+  const retrieveImage = (url) => {
+    fetch(url, { mode: 'cors' })
+      .then((response) => response.json())
+      .then((response) => {
+        img.src = response.data.images.original.url;
+      });
+  };
+
+  const userInput = () => {
+    const searchButton = document.querySelector('.search');
+
+    searchButton.addEventListener('click', (event) => {
+      const inputField = document.querySelector('#input');
+      let enteredValue = '';
+
+      event.preventDefault();
+      enteredValue = inputField.value;
+      retrieveImage(setURL(enteredValue));
+    });
+  };
+
+  const newGif = () => {
+    refreshButton.addEventListener('click', () => {
+      retrieveImage(setURL('cat'));
+    });
+  };
+
+  retrieveImage(setURL(userInput()));
+  newGif();
+};
+
+retrieveGif();
