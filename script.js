d219367c87b541c92e7c51091724543e7d1908c1
Carlos Diaz <cdiaz86@gmail.com>
2020-02-28 19:44:19 -0500

    Adds button to refresh page.

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const img = document.querySelector('img');
+const refreshButton = document.querySelector('.refresh-button');
 
 fetch('https://api.giphy.com/v1/gifs/translate?api_key=JCi4z2AIWowzJTWX7HtbX7OjQIeZEHpT&s=cats', { mode: 'cors' })
   .then((response) => response.json())
   .then((response) => {
-    console.log(response);
+    img.src = response.data.images.original.url;
   });
+
+refreshButton.addEventListener('click', () => {
+  window.location.reload();
+});
